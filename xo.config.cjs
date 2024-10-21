7ed3ecd357d0293430843f4d9b9f1f1236aa6165
Tim Beadle <tim@takkconsulting.co.uk>
2024-10-21 17:57:56 +0100

    build: Add xo.config.cjs to linting & fix issues

--- xo.config.cjs
+++ xo.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
 	rules: {
-		"arrow-parens": 0,
-		"import/extensions": 0,
-		"n/prefer-global/process": 0,
-		"object-curly-spacing": 0,
-		"unicorn/no-anonymous-default-export": 0,
-		"unicorn/no-typeof-undefined": 0,
-		"unicorn/prefer-module": 0,
-		"unicorn/prefer-string-replace-all": 0,
-		"unicorn/prevent-abbreviations": 0,
-	}
+		'arrow-parens': 0,
+		'import/extensions': 0,
+		'n/prefer-global/process': 0,
+		'object-curly-spacing': 0,
+		'unicorn/no-anonymous-default-export': 0,
+		'unicorn/no-typeof-undefined': 0,
+		'unicorn/prefer-module': 0,
+		'unicorn/prefer-string-replace-all': 0,
+		'unicorn/prevent-abbreviations': 0,
+	},
 };
